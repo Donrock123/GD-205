04d72d9e44aa004c2bf9b1f49b5f5ea92943a81b
Donrock123 <donnellthornill@gmail.com>
2019-02-13 16:43:40 -0500

    Broad Movement

---
+++ Assets/BoradMovment.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoradMovment: MonoBehaviour {
+    Vector3 playerPos;
+    public Transform destination;
+    public hazard; 
+
+    // Start is called before the first frame update
+    void Start() {
+        playerPos = transform.position;   
+
+}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerPos += transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerPos += transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerPos -= transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerPos -= transform.right;
+        }
+        if (playerPos.x == destination.position.x &&
+            playerPos.z == destination.position.z){
+            playerPos += transform.up;
+        }
+
+        if (playerPos.x == hazard.position.x &&
+            playerPos.z == hazard.position.z)
+
+        transform.position = playerPos;
+
+    }
+}
